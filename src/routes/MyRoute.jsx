b5afa21f8feb0a47b3b487b153cdb2d082f1784b
SHiuN26 <justodme26@gmail.com>
2024-06-04 13:40:19 +0800

    asd

--- src/routes/MyRoute.jsx
+++ src/routes/MyRoute.jsx
@@ -1,21 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "@/layout/AppLayout";
 import Resume from "@/pages/ResumePage/Resume";
 import PDF from "@/pages/PDFPage/PDF";
 import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
-import { useLocation } from "react-router-dom";
-const routePrefix = process.env.REACT_APP_ROUTE_PREFIX || "";
 
+// const routePrefix = process.env.REACT_APP_ROUTE_PREFIX || "";
+// basename={routePrefix}
 const MyRoute = () => {
-  const location = useLocation();
-  const [currentRoute, setCurrentRoute] = useState(location.pathname);
-
-  useEffect(() => {
-    console.log("currentRoute", currentRoute);
-  }, [currentRoute]);
   return (
-    <Routes basename={routePrefix}>
+    <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Resume />} />
         <Route path="pdf" element={<PDF />} />
