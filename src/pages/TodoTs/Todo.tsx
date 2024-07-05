76ce78a44c2e1f3d02f3d00c8e991e6c484dc4cb
SHiuN26 <justodme26@gmail.com>
2024-07-05 16:11:12 +0800

    把dist靜態複製到 docs

--- src/pages/TodoTs/Todo.tsx
+++ src/pages/TodoTs/Todo.tsx
@@ -4,10 +4,12 @@ import { Button } from 'antd'
 
 const Todo:React.FC = () => {
     const [todos, setTodos] = useState<TodoItem[]>([])
-  
+    const [count ,setCount] = useState<number>(0);
 
     return (
       <>
+      <Button onClick={()=>setCount(count +1)}>點我加一</Button>
+      計數器: {count}
         總代辦事項數量: {todos.length}
       </>
     )
